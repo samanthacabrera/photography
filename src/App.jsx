02b3e86cd7e50f1ec2547c174f07139f9b8f7517
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-18 13:12:20 -0700

    install and set up tailwind css complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Gallery from "./Gallery";
+import './App.css'
 
 function App() {
-
-
   return (
     <>
-    <h1>Photography Porfolio</h1>
+    <h1 className="text-white bg-black">Photography Portfolio</h1>
+    <Gallery/>
     </>
   )
 }
---
+++ src/Gallery.jsx
@@ -0,0 +1,9 @@
+const Gallery = () => {
+    return (
+        <div className="grid grid-cols-2 lg:gird-cols-1">
+          
+        </div>
+    )
+}
+
+export default Gallery;
